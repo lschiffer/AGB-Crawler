9e72cb5b6d5e3cab402e13eca72e107b12691b1d
Lena Schiffer <lschiffer@universe-factory.net>
2015-11-20 12:39:08 +0100

    parser writes raw HTML, XML and crawl date into database

--- parser.py
+++ parser.py
@@ -9,6 +9,9 @@ from socket import timeout
 from bs4 import BeautifulSoup
 from bs4 import Tag, NavigableString
 
+from datetime import datetime
+
+
 def main():
 
     new_database()
@@ -33,7 +36,7 @@ def main():
             url = row[1]
 
             (page, xml) = parse_url(app_id, url)
-            print(xml, "\n")
+            print(xml, "\n\n\n")
 
             with output_con:
 
@@ -42,11 +45,21 @@ def main():
                 output_cursor.execute("INSERT OR IGNORE INTO AGB (app_id,text_url) VALUES ('{id}','{text_url}')".\
                 format(id=app_id, text_url=url))
 
-                #output_cursor.execute("INSERT OR IGNORE INTO AGB (app_id,text_raw) VALUES ('{id}','{raw}')".\
-                #format(id=app_id, raw=page))
+                output_cursor.execute("UPDATE AGB SET text_crawldate=('{date}') WHERE app_id='{id}'".\
+                format(id=app_id, date=datetime.isoformat(datetime.now())))
 
-                # TODO: write text_raw, text_xml and text_crawldate in
-                # database
+                output_cursor.execute("UPDATE AGB SET text_raw=('{raw}') WHERE app_id='{id}'".\
+                format(id=app_id, raw=escape(page)))
+
+                output_cursor.execute("UPDATE AGB SET text_xml=('{text_xml}') WHERE app_id='{id}'".\
+                format(id=app_id, text_xml=escape(xml)))
+
+                #TODO: write into existing database without creating a new one every time
+
+
+def escape(text):
+
+    return text.replace("'", "''")
 
 
 def new_database():
@@ -56,23 +69,30 @@ def new_database():
     with output_con:
 
        cur =  output_con.cursor()
-       cur.execute("DROP TABLE IF EXISTS AGB")
-       cur.execute("CREATE TABLE AGB(app_id TEXT PRIMARY KEY, text_url TEXT, text_raw TEXT, text_xml TEXT, crawler_name TEXT, text_type TEXT, text_crawldate DATE, check_auto BOOL, check_man BOOL, text_quality INTEGER, app_name TEXT, app_storename TEXT, app_permissions TEXT)")
+       cur.execute('''DROP TABLE IF EXISTS AGB''')
+       cur.execute('''CREATE TABLE AGB(app_id TEXT PRIMARY KEY, text_url
+       TEXT, text_raw TEXT, text_xml TEXT, crawler_name TEXT, text_type
+       TEXT, text_crawldate TEXT, check_auto BOOL, check_man BOOL,
+       text_quality INTEGER, app_name TEXT, app_storename TEXT,
+       app_permissions TEXT)''')
 
 
 def parse_url(app_id, url):
 
     page = ''
 
+    # TODO: test exception handling
+
     try:
         page = urlopen(url, timeout = 5)
 
     except (HTTPError, URLError) as error:
         print("error", error, "at url", url)
-    except timeout:
-        print("timeout at url", url)
     else:
         print("opening", url)
+
+    if page == '':
+        return ('','')
 
     soup = BeautifulSoup(page, "lxml")
     xml_output = "<dse>"
