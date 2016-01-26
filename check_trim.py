21dc3c29c29ec98b186f995da8bc26f48c29856d
Svende <svende@abwesend.de>
2016-01-26 21:47:52 +0100

    Better functions and english terms

--- check_trim.py
+++ check_trim.py
@@ -26,44 +26,11 @@ def check_keywords(xml):
     @type xml: string
     @return: true of false
     """
-    n = -1
-    e=False
-    var1 = xml.find("Datenschutz")
-    var2 = xml.find("Allgemeine Geschäftsbedingungen")
-    var3 = xml.find("Privacy")
-    var4 = xml.find("DSE")
-    var5 = xml.find("AGB")
-    var6 = xml.find("DATENSCHUTZ")
-    var7 = xml.find("PRIVACY")
 
-    if var1 > n:
-        e=True
-    if var2 > n:
-        e=True
-    if var3 > n:
-        e=True
-    if var4 > n:
-        e=True
-    if var5 > n:
-        e=True
-    if var6 > n:
-        e=True
-    if var7 > n:
-        e=True
-    return e
-
-def minimum(liste):
-
-
-    min = liste[0]
-    i = 1
-    while i < len(liste):
-        if liste[i] < min:
-            min = liste[i]
-        i = i + 1
-
-    return min
-
+    xml = xml.lower()
+    keywords = ["datenschutz", "allgemeine geschäftsbedingungen", "privacy", "dse", "agb"]
+    return (any(x in xml for x in keywords))
+    
 def cutting(xml):
     """Check if there are curly braces inside given xml-string
 
@@ -72,77 +39,64 @@ def cutting(xml):
     @return: true of false
     """
     #Anfangsvariablen setzen
-    ausgang=xml
-    anfang=-1
-    ende=-1
-    gefunden=-1
+    raw_text=xml
+    xml_lower=xml.lower()
+    begin=-1
+    end=-1
+    first_word_found=-1
     text ='<?xml version="1.0" encoding="utf-8"?>\n<dse>\n <para>\n  '
-    endergebnis=ausgang
+    final_result=raw_text
+    keywords = ["datenschutz", "allgemeine geschäftsbedingungen", "privacy", "dse", "agb"]
 
     while True:
-        #Suche das Erste Wort title
-        anfang = xml.find("<title>")
-        #print(anfang)
-        #Suche das 2te Wort title
-        ende = xml.find("</title>")
-        #print(ende)
-        #Suche nach den Woertern und wenn nicht im Text setze sie auf die Länge des Textes plus 1 (also hinter den Text sozusagen)
-        gefunden1 = xml.find("Datenschutz")
-        if gefunden1 == -1:
-            gefunden1=len(xml)+1
-        gefunden2 = xml.find("DATENSCHUTZ")
-        if gefunden2 == -1:
-            gefunden2=len(xml)+1
-        gefunden3 = xml.find("Allgemeine Geschäftsbedingungen")
-        if gefunden3 == -1:
-            gefunden3=len(xml)+1
-        gefunden4 = xml.find("Privacy")
-        if gefunden4 == -1:
-            gefunden4=len(xml)+1
-        gefunden5 = xml.find("DSE")
-        if gefunden5 == -1:
-            gefunden5=len(xml)+1
-        gefunden6 = xml.find("AGB")
-        if gefunden6 == -1:
-            gefunden6=len(xml)+1
-        gefunden7 = xml.find("PRIVACY")
-        if gefunden7 == -1:
-            gefunden7=len(xml)+1
-        #Suche den Kleinsten Wert, dies ist das Wort as zuerst geprüft wird, ob es im title steht
-        #print(gefunden1, " ", gefunden2, " ", gefunden3, " ", gefunden4, " ", gefunden5, " ", gefunden6, " ", gefunden7, "\n")
-        gefunden= minimum([gefunden1, gefunden2, gefunden3, gefunden4, gefunden5, gefunden6, gefunden7])
-        #print(gefunden)
+        indizes=[]
+        begin = xml.find("<title>")
+        end = xml.find("</title>")
+        for i in keywords:
+            a= xml_lower.find(i)
+            if a == -1:
+                indizes += [float('inf')]
+            else:
+                indizes += [a]
 
+        first_word_found=min(indizes)
+       
 
         #Der Text soll mind noch 2000 Wörter haben, wenn weniger soll nicht gekürzt werden
         if len(xml)<2000:
-            endergebnis=ausgang
+            final_result=raw_text
             break
 
-        #Wenn das gefundene Wort zwischen den title steht, dann kürze alles vor dem title weg und nimm den rest als Ergebnis
-        if gefunden<ende:
-            if gefunden>anfang:
-                ergebnis= xml[anfang:]
-                endergebnis=text+ergebnis
-                #print(endergebnis)
+        #Wenn das gefundene Wort zwischen den title steht, dann kürze alles vor dem title weg und nimm den rest als result
+        if first_word_found<end:
+            if first_word_found>begin:
+                result= xml[begin:]
+                final_result=text+result
+                #print(final_result)
                 break
 
         #Wenn das gefundene Wort hinter dem title steht gibt es 2 Varianten: Die erste ist es steht noch vor dem Ende des Textes, dann wird alles incl der 2 title rausgekürzt
         #Die zweite es steht außerhalb des textes, was dadurch passieren kann weil oben alle nicht gefundenen Wörter auf len+1 gesetzt wurden, dann soll es abbrechen, dann gibt es keines der Wörter weiter im Text
-        if gefunden>ende:
-            if gefunden>len(xml):
-                endergebnis=ausgang
+        if first_word_found>end:
+            if first_word_found>len(xml):
+                final_result=raw_text
                 break
-            xml= xml[(ende+8):]
+            xml= xml[(end+8):]
+            xml_lower= xml[(end+8):]
 
         #Wenn das Wort in einem text vor dem title steht wird dieser text weggekürzt
-        if gefunden<anfang:
-            #xml= xml[anfang:]
-            endergebnis = ausgang
+        if first_word_found<begin:
+            #xml= xml[begin:]
+            final_result = raw_text
             break
 
-    #queue.put(endergebnis)
-    return endergebnis
+        if begin == -1:
+            if end ==-1:
+                final_result=raw_text
+                break
+
+    #queue.put(final_result)
+    return final_result
 
 def main(database = "parsed_all.db"):
     """main
